2d6c451d05fa30c59ac05ec61b4127e1b6c1dbfa
Duke2 <jplaroche_2000@yahoo.ca>
2020-02-05 17:38:43 -0500

    Update pubsub_multiplexor.py

--- CloudFunctions/pubsub_multiplexor.py
+++ CloudFunctions/pubsub_multiplexor.py
@@ -2,21 +2,27 @@ import base64
 import json
 
 def pubsub_multiplexor(event, context):
+
     """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
+    
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     json_string = pubsub_message[1:-1]
-    """print(json_string)"""    
     json_object = json.loads(json_string)
-    """"print(json_object)"""
-    print(json_object['metadata']['TableName'])
+
+    table_name = json_object['metadata']['TableName']
+    print(table_name)
+
+    operation_name = json_object['metadata']['OperationName']
+    print(operation_name)
+    
     domain_object = json_object['data']
     print(domain_object)
-    """
-    print(customer['LAST_NAME'])
-    print(customer['NAME'])
-    print(customer['ID'])
-    """
+
+    if table_name == 'SCOTT.CUSTOMER':    
+        print(domain_object['LAST_NAME'])
+        print(domain_object['NAME'])
+        print(domain_object['ID'])
