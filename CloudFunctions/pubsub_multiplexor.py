f1f0a77944824ca6a393c4378ba3b287529bffa5
Duke2 <jplaroche_2000@yahoo.ca>
2020-02-06 12:28:48 -0500

    Update pubsub_multiplexor.py

--- CloudFunctions/pubsub_multiplexor.py
+++ CloudFunctions/pubsub_multiplexor.py
@@ -23,6 +23,31 @@ def pubsub_multiplexor(event, context):
     print(domain_object)
 
     if table_name == 'SCOTT.CUSTOMER':    
-        print(domain_object['LAST_NAME'])
-        print(domain_object['NAME'])
         print(domain_object['ID'])
+        print(domain_object['NAME'])
+        print(domain_object['LAST_NAME'])
+
+    elif table_name == 'SCOTT.ADDRESS':
+        print(domain_object['ID'])
+        print(domain_object['STREET_NUMBER'])
+        print(domain_object['STREET'])    
+        print(domain_object['CITY'])    
+
+    elif table_name == 'SCOTT.ADDRESS_LINK':
+        print(domain_object['ID'])
+        print(domain_object['ADDRESS_ID'])
+        print(domain_object['CUSTOMER_ID'])     
+
+    elif table_name == 'SCOTT.CUSTOMER_ORDER':
+        print(domain_object['ID'])
+        print(domain_object['ORDER_DATE'])
+        print(domain_object['CUSTOMER_ID'])     
+        print(domain_object['TOTAL'])     
+
+    else:
+        print(domain_object['ID'])
+        print(domain_object['CUSTOMER_ORDER_ID'])
+        print(domain_object['DESCRIPTION'])     
+        print(domain_object['QUANTITY'])     
+        print(domain_object['PRICE'])   
+         
