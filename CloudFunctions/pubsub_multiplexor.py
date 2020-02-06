ebae12819d3b7349e3154f34e5dc9081dfebeb50
Duke2 <jplaroche_2000@yahoo.ca>
2020-02-06 15:15:41 -0500

    Update pubsub_multiplexor.py

--- CloudFunctions/pubsub_multiplexor.py
+++ CloudFunctions/pubsub_multiplexor.py
@@ -47,33 +47,35 @@ def pubsub_multiplexor(event, context):
     
     datastore_client = create_client('telus-project-001');
 
-    if table_name == 'SCOTT.CUSTOMER':    
-        print(domain_object['ID'])
-        print(domain_object['NAME'])
-        print(domain_object['LAST_NAME'])
-        add_customer(datastore_client, domain_object['ID'], domain_object['NAME'], domain_object['LAST_NAME'])
+    if operation_name == 'INSERT':
 
-    elif table_name == 'SCOTT.ADDRESS':
-        print(domain_object['ID'])
-        print(domain_object['STREET_NUMBER'])
-        print(domain_object['STREET'])    
-        print(domain_object['CITY'])    
+        if table_name == 'SCOTT.CUSTOMER':    
+            print(domain_object['ID'])
+            print(domain_object['NAME'])
+            print(domain_object['LAST_NAME'])
+            add_customer(datastore_client, domain_object['ID'], domain_object['NAME'], domain_object['LAST_NAME'])
 
-    elif table_name == 'SCOTT.ADDRESS_LINK':
-        print(domain_object['ID'])
-        print(domain_object['ADDRESS_ID'])
-        print(domain_object['CUSTOMER_ID'])     
+        elif table_name == 'SCOTT.ADDRESS':
+            print(domain_object['ID'])
+            print(domain_object['STREET_NUMBER'])
+            print(domain_object['STREET'])    
+            print(domain_object['CITY'])    
 
-    elif table_name == 'SCOTT.CUSTOMER_ORDER':
-        print(domain_object['ID'])
-        print(domain_object['ORDER_DATE'])
-        print(domain_object['CUSTOMER_ID'])     
-        print(domain_object['TOTAL'])     
+        elif table_name == 'SCOTT.ADDRESS_LINK':
+            print(domain_object['ID'])
+            print(domain_object['ADDRESS_ID'])
+            print(domain_object['CUSTOMER_ID'])     
 
-    else:
-        print(domain_object['ID'])
-        print(domain_object['CUSTOMER_ORDER_ID'])
-        print(domain_object['DESCRIPTION'])     
-        print(domain_object['QUANTITY'])     
-        print(domain_object['PRICE'])   
+        elif table_name == 'SCOTT.CUSTOMER_ORDER':
+            print(domain_object['ID'])
+            print(domain_object['ORDER_DATE'])
+            print(domain_object['CUSTOMER_ID'])     
+            print(domain_object['TOTAL'])     
+
+        else:
+            print(domain_object['ID'])
+            print(domain_object['CUSTOMER_ORDER_ID'])
+            print(domain_object['DESCRIPTION'])     
+            print(domain_object['QUANTITY'])     
+            print(domain_object['PRICE'])   
          
